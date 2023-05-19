5cff054b7baba1274d945c2bbd3fd60c4b24e53a
Jamoldin2003 <jamoldin200310@gmail.com>
2023-05-19 14:51:51 +0500

    Final Commit

--- app/src/main/java/com/example/mpproject/MainActivity.java
+++ app/src/main/java/com/example/mpproject/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.mpproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.mpproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.registerBtn.setOnClickListener(view->{
+            startActivity(new Intent(this, RegisterActivity.class));
+        });
+
+        binding.loginBtn.setOnClickListener(view->{
+            startActivity(new Intent(this,LoginActivity.class));
+        });
+
+
     }
 }
